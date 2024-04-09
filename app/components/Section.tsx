10a4c607d62a88f2871ad6cc34fbe25cfa4d7920
CollinsChacha <chachacollinsmugendi@gmail.com>
2024-04-10 02:47:58 +0300

    removed window bug

--- app/components/Section.tsx
+++
@@ -1,8 +0,0 @@
-// Section.js
-import React from "react";
-
-const Section = ({ children }: any) => {
-  return <div className="flex-1 overflow-y-auto h-screen">{children}</div>;
-};
-
-export default Section;
--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,31 @@
 import Image from "next/image";
 import Textgenerator from "./components/Textgenerator";
 import hero from "../public/hero.png";
-import Section from "./components/Section";
-import Layout from "./components/ui/resize";
 
 export default function Home() {
   return (
-    <Layout>
-      <Section>
-        <div className="grid-halves h-screen-navbar">
-          <div className="bg-teal border-right">
-            <div className="column-padding">
-              <div className="tablet-centered">
-                <div className="content-grid home-hero">
-                  <Textgenerator />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="bg-salmon">
-            <div className="column-padding centered">
-              <div className="callout-wrap">
-                <Image
-                  className="callout-image"
-                  src={hero}
-                  alt="friendly people"
-                  placeholder="blur"
-                ></Image>
-              </div>
+    <div className="grid-halves h-screen-navbar">
+      <div className="bg-teal border-right">
+        <div className="column-padding">
+          <div className="tablet-centered">
+            <div className="content-grid home-hero">
+              <Textgenerator />
             </div>
           </div>
         </div>
-      </Section>
-    </Layout>
+      </div>
+      <div className="bg-salmon">
+        <div className="column-padding centered">
+          <div className="callout-wrap">
+            <Image
+              className="callout-image"
+              src={hero}
+              alt="friendly people"
+              placeholder="blur"
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
