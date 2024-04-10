5e4fff54bd58277cac80758628693fb59babb3a0
CollinsChacha <chachacollinsmugendi@gmail.com>
2024-04-10 13:14:38 +0300

    added loading animation

---
+++ app/components/LoadingAnime.tsx
@@ -0,0 +1,5 @@
+import "./loadinganime.css";
+
+export const LoadingAnime = () => {
+  return <div className="loader"></div>;
+};
---
+++ app/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingAnime } from "./components/LoadingAnime";
+
+export default function Loading() {
+  return <LoadingAnime />;
+}
